a61f291c31d6f3f4d3486cbc772f25e94f18ceae
Awaluddin <awal14h@gmail.com>
2022-08-23 14:50:46 +0800

    mengganti readme rst

--- application/views/layouts/home.php
+++ application/views/layouts/home.php
@@ -1,1 +1,6 @@
-<h1>Hello</h1>
+<section>
+    <div class="container">
+        <h1>Hi Folks, Welcome</h1>
+        <p>This is a source coding about CRUD method in codeigniter 3</p>
+    </div>
+</section>
