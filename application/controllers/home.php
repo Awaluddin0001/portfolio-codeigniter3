7f18dad2810241d81c720bd1b91729fe8d6cb175
Awaluddin <awal14h@gmail.com>
2022-08-23 11:29:58 +0800

    make a view page

---
+++ application/controllers/home.php
@@ -0,0 +1,18 @@
+<?php 
+
+class Home extends CI_Controller {
+    public function index() {
+
+        $data['title'] = 'Home Page';
+
+        $this->load->view('templates/head', $data);
+        $this->load->view('templates/header');
+
+        $this->load->view('layouts/home');
+
+        $this->load->view('templates/footer');
+    }
+}
+
+
+?>
---
+++ application/views/templates/head.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=1200px, initial-scale=1.0">
+    
+    <!-- Google Font -->
+    <!-- Roboto - sans serif & DM Serif Display-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    
+    <!-- CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/main.css">
+    
+    <!-- Javascript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <script src="<?= base_url() ?>assets/css/main.js"></script>
+    
+    <link rel="iconPage" href="<?= base_url() ?>assets/images/favicon.ico" type="image/x-icon">
+    <title><?= $title ?></title>
+</head>
